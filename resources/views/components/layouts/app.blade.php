fe2e3b245c34fb53cf91dd220c7636c397b5c12c
signnd <ngurah@tutamail.com>
2025-12-22 16:49:13 +0800

    fix(layout): apply .flux-layout desktop grid to align sidebar and main

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -1,5 +1,17 @@
-<x-layouts.app.sidebar :title="$title ?? null">
-    <flux:main>
-        {{ $slot }}
-    </flux:main>
-</x-layouts.app.sidebar>
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="dark">
+    <head>
+        @include('partials.head')
+        @livewireStyles
+    </head>
+    <body class="flux-layout min-h-screen bg-white dark:bg-zinc-800">
+        <x-layouts.app.sidebar :title="$title ?? null" />
+
+        <flux:main>
+            {{ $slot }}
+        </flux:main>
+
+        @fluxScripts
+        @livewireScripts
+    </body>
+</html>
